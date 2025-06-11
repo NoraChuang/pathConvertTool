f5f3ca758a52f02b98e92ab6e5c5a3c30b9b6ccc
Nora <kiki841112@gmail.com>
2025-06-11 22:59:11 +0800

    修正註解

--- convert_path.py
+++ convert_path.py
@@ -1,7 +1,7 @@
 def main():
     # 1. 輸入轉換模式
     mode_choice = input("請選擇轉換模式：\n1. Mac路徑轉Windows路徑\n2. Windows路徑轉Mac路徑\n請輸入1或2：")
-    # Mac to Windows
+    # Mac to Windows 用1來判斷
     if mode_choice == '1': 
         ori_path = input("請輸入欲轉換的Mac資料夾路徑：\n")
         # 2. 將/轉換成\
